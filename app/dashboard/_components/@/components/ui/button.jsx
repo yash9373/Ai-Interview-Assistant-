527267f57dd528541389363c5d59c2cbec4ba195
piyush-ghanghav <131697699+piyush-ghanghav@users.noreply.github.com>
2024-12-02 01:04:18 +0530

    Add InterviewList Component & Button Styling

--- app/dashboard/_components/@/components/ui/button.jsx
+++ app/dashboard/_components/@/components/ui/button.jsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-blue-500 text-white hover:bg-blue-600",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
